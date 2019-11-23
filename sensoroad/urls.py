0126a755661bdcb7409bba7efd5833af82fd06b8
webguru <webdev1028@gmail.com>
2019-11-23 14:59:38 -0300

    drop-down, layers

--- sensoroad/urls.py
+++ sensoroad/urls.py
@@ -25,6 +25,7 @@ from sensoroad.apps.api import urls as api_urls
 from sensoroad.apps.user.forms import UserLoginForm
 from sensoroad.apps.dashboard.views import dashboard_view
 from sensoroad.apps.user.views import SignUpView
+from sensoroad.apps.dashboard.views import city_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -37,5 +38,7 @@ urlpatterns = [
         template_name="login.html", authentication_form=UserLoginForm), name='login'),
     url('^register/$', SignUpView.as_view(), name='register'),
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/assets/img/favicon.png')),
+
+    path('maps/<str:sel_city_state>', city_view)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
